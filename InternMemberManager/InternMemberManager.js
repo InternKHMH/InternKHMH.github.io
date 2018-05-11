53dc328ffab30841fb8f1f49fd6a41d7fb295af3
khanh pham <pham van khanh@DESKTOP-1SRG42G>
2018-05-11 15:39:34 +0700

    khanh them giao dien trang intern member manager 10/5/2017

---
+++ InternMemberManager/InternMemberManager.js
@@ -0,0 +1,40 @@
+﻿ $(function(){
+ 	$('.tbUser tbody tr').click(function(event) {
+ 		event.preventDefault();
+ 		$('.detailUser').addClass('detailUserShow');
+ 		$('.den').addClass('hienthi');
+
+ 		var bien=$('.tbUser tr').data('iduser');
+ 		alert(bien[1]);
+
+
+ 	});
+ 	$('.nutthoat').click(function(event){
+ 		$('.detailUser').addClass('detailAndi');
+ 		$('.den').removeClass('hienthi');
+ 		$('.detailUserShow').removeClass('detailUserShow');
+ 		$('.detailUser').removeClass('detailAndi');
+
+ 		$.ajax({
+ 			url: '/path/to/file',
+ 			type: 'POST',
+ 			dataType: 'json',
+ 			data: {param1: 'value1'},
+ 		})
+ 		.done(function() {
+ 			console.log("success");
+ 		})
+ 		.fail(function() {
+ 			console.log("error");
+ 		})
+ 		.always(function(res) {
+ 			console.log("complete");
+ 			
+
+
+ 		});
+ 		
+
+ 	})
+})  
+ 
